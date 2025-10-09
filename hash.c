47bd030766669b01a66b5d656e93db685d1c1cc2
Sean P. Goggins <s@goggins.com>
2025-10-09 17:18:27 -0500

    Comments Added related to Instructions
    Some students had trouble reading the instructions, so I annoted hash.c with those values.

--- hash.c
+++ hash.c
@@ -6,18 +6,24 @@
 
 // These are stubs.  That is, you need to implement these functions.
 
+// return -1 when a collision is encountered. (i.e., you don't need to use chaining or
+// open addressing here. Just return -1) 
 int InsertFailCollision(	void * hashtable, int elementSize, int elementCountMax,
                             int key, void * element, int (*HashFunc)(int key))
 {
     return 0;
 }
 
+// similar to `InsertFailCollision`, your search function may assume there are 
+// NO chained or open addressed record in this case
 void * SearchNoCollision(void * hashtable, int key, int elementSize, int (*HashFunc)(int key))
 {
     return NULL;
 }
 
-// div and multi: Clarify
+// div and multi: hash the value using a function that is division, and another
+// that is multiplication based.  Note that insertion and search will also need to 
+// use this hashing function. 
 int DivMethod(int key)
 {
 	return 0;
@@ -29,6 +35,8 @@ int MultMethod(int key)
 }
 
 
+// All of these remaining methods require you to deal with chained values in the case
+// That collisions occur. (Obviously) check to see if there is a collision, and handle it
 void * AllocateChainTable(int elementCountMax)
 {
     return NULL;
